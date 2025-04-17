e7f2c77123677dc62676277e85ac1b0df38d36c3
Joseph Ligier <joseph.ligier@accenture.com>
2025-04-17 16:49:29 +0200

    Add verify

--- aya-prod/aya-tracing/tracepoint-exit/step1/verify.sh
+++ aya-prod/aya-tracing/tracepoint-exit/step1/verify.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 docker container ls -f name=aya -q | wc -l | grep '^1$'
+docker exec -it aya cargo build
+docker exec -it aya ls tracepoint-binary-ebpf/src/hook_exit.rs
+docker exec -it aya grep 'mod hook_exit' tracepoint-binary-ebpf/src/main.rs 
+docker exec -it aya grep 'sys_exit_execve' tracepoint-binary/src/main.rs 
+
