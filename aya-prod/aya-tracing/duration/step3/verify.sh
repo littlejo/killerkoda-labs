69b732b47f54821fa9d23ce3fabe567658e64a65
Joseph Ligier <joseph.ligier@accenture.com>
2025-04-17 17:23:08 +0200

    Improve

--- aya-prod/aya-tracing/duration/step3/verify.sh
+++ aya-prod/aya-tracing/duration/step3/verify.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+
+docker container ls -f name=aya -q | wc -l | grep '^1$'
+docker exec aya cargo build
+docker exec aya grep 'bpf_get_current_pid_tgid' tracepoint-binary-ebpf/src/hook.rs
+docker exec aya grep 'bpf_get_current_pid_tgid' tracepoint-binary-ebpf/src/hook_exit.rs
+docker exec aya grep 'LruHashMap' tracepoint-binary-ebpf/src/common.rs
