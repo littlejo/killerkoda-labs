c611f5763eefb24278c209ad02be262d5ae50839
root <root@bd99c3edc11c.mylabserver.com>
2025-08-28 06:36:56 +0000

    First commit for uprobe lab

---
+++ aya-prod/aya-uprobe/program/step2/background.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/littlejo/aya-examples/refs/heads/main/tracepoint-binary/Cargo.lock -O /root/aya-test/Cargo.lock
---
+++ aya-prod/aya-uprobe/setup.sh
@@ -0,0 +1,1 @@
+docker pull littlejo/aya:2.0.0
